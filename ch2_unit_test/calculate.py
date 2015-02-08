4c87a2f1fb5d83e6e7fbb2c22fb2251a099ab552
Manu <bionikspoon@gmail.com>
2015-02-08 01:12:45 -0600

    ch4 doctest

--- ch2_unit_test/calculate.py
+++ ch2_unit_test/calculate.py
@@ -1,5 +1,23 @@
 class Calculate(object):
     def add(self, x, y):
+        """
+        Takes two integers and adds them together to produce the result.
+        >>> c = Calculate()
+        >>> c.add(1,1)
+        2
+
+        >>> c.add(25, 125)
+        150
+
+        >>> c.add(1.0, 1.0)
+        Traceback (most recent call last):
+        ...
+        TypeError: Invalid type: <type 'float'> and <type 'float'>
+
+        :param x:
+        :param y:
+        :return: :raise TypeError:
+        """
         if type(x) == type(y) == int:
             return x + y
         else:
@@ -9,4 +27,4 @@ class Calculate(object):
 if __name__ == '__main__':  # pragma: no cover
     calc = Calculate()
     result = calc.add("Hello", "World")
-    print result
+    # print result
