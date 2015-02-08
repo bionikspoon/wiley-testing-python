be9399f61ef1065c0afa8d4b422cdd232df50551
Manu <bionikspoon@gmail.com>
2015-02-08 12:48:23 -0600

    ch5 tdd

---
+++ ch5_tdd/test/account_test.py
@@ -0,0 +1,15 @@
+import unittest
+from ch5_tdd.account import Account
+
+
+class TestAccount(unittest.TestCase):
+    # def test_account_object_can_be_created(self):
+    #     account = Account()
+
+    def test_account_object_returns_current_balance(self):
+        account = Account("001", 50)
+        self.assertEqual(account.account_number, "001")
+        self.assertEqual(account.balance, 50)
+
+if __name__ == '__main__':
+    unittest.main()
